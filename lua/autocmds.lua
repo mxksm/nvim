43cb4d84f1a9298cb8975b6f4457944c9b9f5c32
Maksim <mlavrenk@purdue.edu>
2026-02-15 20:42:13 -0500

    update

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -15,14 +15,3 @@ autocmd("FileType", {
     vim.opt_local.expandtab = true
   end,
 })
-
------------------------------------------------------------
--- 🪄 AUTO OPEN/CLOSE QUICKFIX WITH VIMTEX EVENTS
------------------------------------------------------------
-
-vim.cmd([[
-  augroup VimtexQuickfixToggle
-    autocmd!
-    autocmd User VimtexEventCompileSuccess,VimtexEventCompileFailed lua if vim.g.quickfix_open == true then vim.cmd("copen") else vim.cmd("cclose") end
-  augroup END
-]])
